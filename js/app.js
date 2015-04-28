7cea94a454abc654b4860af4bcdd727fce1bb28d
Blake Johnson <blake41@gmail.com>
2015-04-28 11:00:21 -0400

    added notes to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// you can ignore this file, but it gives you an example of how you might actually run this code in the browser
+// if you run 'python -m SimpleHTTPServer' from the root directory it will load the tictactoe.html file.  that file has script tags which loads the required js in order
+// those files draw the boards HTML and wire up the event listeners
 var app = app || {};
 app.board = new Board();
 app.board.init();
